a6ecaa571c105e089d9cf9b28802a5a2339a593f
rbrq03 <rbrq03@outlook.com>
2024-08-25 01:10:07 +0000

    add cn version ocr

--- utils/observation.py
+++ utils/observation.py
@@ -1,10 +1,21 @@
 import easyocr
+import cnocr
 
-def get_ocr(image):
 
-    reader = easyocr.Reader(["en"], detect_network="dbnet18", gpu=True)
-    result = reader.readtext(image, detail=0, paragraph=True)
+def get_ocr(image, version="en"):
+
+    if version == "en":
+        reader = easyocr.Reader(["en"], detect_network="dbnet18", gpu=True)
+        result = reader.readtext(image, detail=0, paragraph=True)
+    elif version == "cn":
+        reader = cnocr.CnOcr(context="cuda:0")
+        result = reader.ocr(image)
+        result = ";".join([item["text"] for item in result])
+    else:
+        raise NotImplementedError("This version of OCR model is not implemented.")
+
     return result
+
 
 def get_caption(image, processor, model):
 
